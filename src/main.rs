d2771e8403f5285095e1da6fc55e947884af5550
Ari Krumbein <arkrumbe@microsoft.com>
2020-11-22 23:32:32 -0800

    can read key presses and show numerical value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
+
+#[allow(unused_assignments)]
+
 fn main() {
-    println!("Hello, world!");
+    // assigning so the value won't be dropped.
+    let _stdout = stdout().into_raw_mode().unwrap();
+    for b in io::stdin().bytes() {
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control() {
+            println!("{:?} \r", b);
+        } else {
+            println!("{:?} ({})\r", b, c);
+        }
+        if c == 'q' {
+            break;
+        }
+    }
 }
